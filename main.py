fb2d482b2f5dadf66ece7334a38e70d49919ef1a
nikmosi <i@nikmosi.ru>
2026-03-19 10:53:07 +0700

    feat: check nicknames

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import sys
 import httpx
 from aiolimiter import AsyncLimiter
 
-# Настрой лимит под свои нужды
-limитер = AsyncLimiter(1, 1)  # 1 запрос в 1 секунду
+# 1 запрос в секунду
+limiter = AsyncLimiter(1, 1)
 
 
 async def is_twitch_live(
     nickname: str,
     client: httpx.AsyncClient,
     limiter: AsyncLimiter,
-) -> tuple[bool, str]:
+) -> tuple[str, bool, str]:
     url = f"https://decapi.me/twitch/uptime/{nickname}"
 
     async with limiter:
@@ -30,25 +30,21 @@ async def is_twitch_live(
     lower = text.lower()
 
     if "offline" in lower:
-        return False, text
+        return nickname, False, text
 
     if "not found" in lower or "invalid" in lower or "error" in lower:
-        raise ValueError(f"Сервис вернул ошибку: {text}")
+        raise ValueError(f"{nickname}: сервис вернул ошибку: {text}")
 
-    return True, text
+    return nickname, True, text
 
 
-async def async_main():
-    if len(sys.argv) < 2:
-        print("Использование: python twitch_check.py <nickname>")
-        sys.exit(1)
-
-    nickname = sys.argv[1]
-
+async def check_nickname(
+    nickname: str,
+    client: httpx.AsyncClient,
+    limiter: AsyncLimiter,
+) -> None:
     try:
-        timeout = httpx.Timeout(15.0)
-        async with httpx.AsyncClient(timeout=timeout, follow_redirects=True) as client:
-            online, raw = await is_twitch_live(nickname, client, лимитер)
+        nickname, online, raw = await is_twitch_live(nickname, client, limiter)
 
         if online:
             print(f"{nickname}: ONLINE")
@@ -56,6 +52,27 @@ async def async_main():
         else:
             print(f"{nickname}: OFFLINE")
             print(f"Ответ сервиса: {raw}")
+
+    except Exception as e:
+        print(f"{nickname}: Ошибка: {e}")
+
+
+async def async_main():
+    if len(sys.argv) < 2:
+        print(
+            "Использование: python twitch_check.py <nickname1> <nickname2> <nickname3>"
+        )
+        sys.exit(1)
+
+    nicknames = sys.argv[1:]
+
+    try:
+        timeout = httpx.Timeout(15.0)
+        async with httpx.AsyncClient(timeout=timeout, follow_redirects=True) as client:
+            tasks = [
+                check_nickname(nickname, client, limiter) for nickname in nicknames
+            ]
+            await asyncio.gather(*tasks)
 
     except Exception as e:
         print("Ошибка:", e)
